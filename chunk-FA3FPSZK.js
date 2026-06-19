4bbebe5c884b314924e9a6d4ece60d32cf45d8f4
halo5146929-design <halo5146929@gmail.com>
2026-06-19 20:02:59 +0000

    Deploying to gh-pages from @ halo5146929-design/nav@83260b57ee3ef324dfea14a95e79fa6191770371 🚀

---
+++ chunk-FA3FPSZK.js
@@ -0,0 +1,1 @@
+import{d as a}from"./chunk-YZS6V64Y.js";import{pb as r,xb as e}from"./chunk-6PAG4XX2.js";var n=class t{constructor(i){this.sanitized=i}transform(i){return this.sanitized.bypassSecurityTrustHtml(i)}static \u0275fac=function(m){return new(m||t)(r(a,16))};static \u0275pipe=e({name:"safeHtml",type:t,pure:!0})};export{n as a};
