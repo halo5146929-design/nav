5969926e221ea4c02553035b5439e22a1f9ab2d5
halo5146929-design <halo5146929@gmail.com>
2026-06-19 20:14:30 +0000

    Deploying to gh-pages from @ halo5146929-design/nav@64259b343d2211e6c1a6f0ea8571747a2fff0ced 🚀

---
+++ chunk-7JWC5NUC.js
@@ -0,0 +1,1 @@
+import{d as a}from"./chunk-MI6GUL7A.js";import{pb as r,xb as e}from"./chunk-GTGQPXGS.js";var n=class t{constructor(i){this.sanitized=i}transform(i){return this.sanitized.bypassSecurityTrustHtml(i)}static \u0275fac=function(m){return new(m||t)(r(a,16))};static \u0275pipe=e({name:"safeHtml",type:t,pure:!0})};export{n as a};
